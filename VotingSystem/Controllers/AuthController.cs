eb8350fa03e6c983dc55dbb6dd95d287d3d14350
Paulo Silva <pauloecomercial367@gmail.com>
2024-08-18 21:48:04 -0300

    :sparkles: Login route request

---
+++ VotingSystem/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using VotingSystem.Dtos.Request;
+using VotingSystem.Services;
+
+namespace VotingSystem.Controllers;
+
+[Route("api/v1/auth")]
+public class AuthController : ControllerBase
+{
+    private IdentityService _identityService;
+    public AuthController(IdentityService identityService) => _identityService = identityService;
+
+    [HttpPost("cadastro")]
+    public async Task<IActionResult> Register(UserRegisterRequest request)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var resultado = await _identityService.CadastrarUsuario(request);
+
+        if (resultado.Sucesso)
+            return Ok(resultado);
+        else if (resultado.Errors.Count > 0)
+            return BadRequest(resultado);
+
+        return StatusCode(500, new { error = "Internal server error" });
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(UserLoginRequest request)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var resultado = await _identityService.Login(request);
+        if (resultado.Sucesso)
+            return Ok(resultado);
+
+        return Unauthorized(resultado);
+    }
+}
